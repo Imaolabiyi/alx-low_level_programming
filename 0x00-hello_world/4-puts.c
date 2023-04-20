9e0ed3c991345376ca0a2c8df036c6e63f8c9d71
imaolabiyi <sheron4krist@yahoo.com>
2023-04-20 09:36:11 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
 	return (0);
 }
