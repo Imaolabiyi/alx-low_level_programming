e9abbfa07b88c0c51eab1060303201c8e3b4c218
imaolabiyi <sheron4krist@yahoo.com>
2023-04-20 09:21:59 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-
-int main() {
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle/n");
-	return 0;
+	return (0);
 }
