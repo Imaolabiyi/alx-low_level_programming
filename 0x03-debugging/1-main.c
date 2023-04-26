275bfdafe9940ec2e6c4686aadd14126aac60c28
imaolabiyi <sheron4krist@yahoo.com>
2023-04-26 07:58:49 -0700

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - avoids an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 * Infinite loop commented out
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
