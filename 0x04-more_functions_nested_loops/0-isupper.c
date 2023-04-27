bbccdea92a6f0580cbd3cbd2f76c0df753a103ae
imaolabiyi <sheron4krist@yahoo.com>
2023-04-27 08:30:37 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_uppercase - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int is_uppercase(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
